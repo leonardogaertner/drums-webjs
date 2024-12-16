bb85772aff189ec8cdab56b529e85041c80876a5
Leonardo Gaertner <leonardo.gaertner@bovenau.com.br>
2024-12-16 17:15:12 -0300

    uptade main

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-document.querySelector('.tecla_pom');
-document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.tagName === 'AUDIO') {
+        elemento.play();
+    } else {
+        console.log('Elemento de áudio não encontrado ou seletor inválido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // Use backticks for template literals
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+}
